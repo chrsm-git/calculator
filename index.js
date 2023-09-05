fbf6163cdf6e345980eb77202ef955502dcb8509
chrsm-git <142023113+chrsm-git@users.noreply.github.com>
2023-09-05 11:49:23 +0800

    files are added

---
+++ index.js
@@ -0,0 +1,28 @@
+const display = document.querySelector(".display");
+const buttons = document.querySelectorAll("button");
+const specialChars = ["%", "*", "/", "-", "+", "="]
+let output = "";
+
+//defines funtion to calculate based on the button clicked
+const calculate = (btnValue) => {
+    if(btnValue === "=" && output !== ""){
+        //If output has %, replace with '/100' before evaluating
+        output = eval(output.replace("%", "/100"));
+    }else if(btnValue === "AC"){
+        output = ""
+    }else if(btnValue == "DEL"){
+        //If DEL button is clicked
+        output = output.toString().slice(0, -1);
+    }else{
+        //If output is empty and button is specialChars the return
+        if(output === "" && specialChars.includes(btnValue)) return;
+        output += btnValue
+    }
+    display.value = output;
+}
+
+//event listener to buttons, call calculate() on click
+buttons.forEach((button) =>{
+    //Button click listener calls calculate() with dataset value as arguement.
+    button.addEventListener("click", (e) => calculate(e.target.dataset.value));
+});
